e5615919268419a1dac580d3711facb54a621a68
Jeff Felchner <accounts+git@thekompanee.com>
2016-08-06 00:21:29 -0500

    Style: Rubocop fixes

--- lib/dirty_url.rb
+++ lib/dirty_url.rb
@@ -1,2 +1,3 @@
+# frozen_string_literal: true
 require 'dirty_url/version'
 require 'dirty_url/railtie' if defined?(Rails)
--- lib/dirty_url/version.rb
+++ lib/dirty_url/version.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module DirtyUrl
-  VERSION = '0.0.4'.freeze
+  VERSION = '0.0.4'
 end
